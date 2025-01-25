f2024f34609a6e41bd077403f3f077538a2121e5
christian caracach <christiancaracach@gmail.com>
2025-01-25 19:23:55 -0300

    Cree auth middleware

---
+++ src/middleware/verifyAuth.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken");
+
+const verifyToken = (req, res, next) => {
+  const bearerHeader = req.headers["authorization"];
+
+  if (!bearerHeader || !bearerHeader.startsWith("Bearer ")) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  const token = bearerHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: "Invalid token" });
+  }
+};
+
+module.exports = verifyToken;
