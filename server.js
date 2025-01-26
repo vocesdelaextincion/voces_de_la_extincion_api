a713a3028d778f23d23d5a8590203c569ebc43d9
christian caracach <christiancaracach@gmail.com>
2025-01-26 11:53:41 -0300

    build scripts

--- server.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config(); 
-const app = require('./src/index'); 
-
-const PORT = process.env.PORT || 3001; 
-
-// Iniciar el servidor
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-});
